e036eb821589dfbcf0212f1a2ef62702b5cf677f
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2023-06-06 12:14:26 -0700

    fixed express routing for different pages

--- server/server.js
+++ server/server.js
@@ -11,8 +11,21 @@ const port = 3000;
 
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use((req, res, next) => {
+  if (req.path.slice(-1) === '/' && req.path.length > 1) {
+    const query = req.url.slice(req.path.length)
+    const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
+    res.redirect(301, safepath + query)
+  } else {
+    next()
+  }
+});
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './../client/dist/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
