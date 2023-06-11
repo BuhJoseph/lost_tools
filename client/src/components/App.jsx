2125395806bdb8635e6ec9c446ea3fc1cd6c8ff1
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2023-06-11 11:13:35 -0700

    reorganized pages into their own folders

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Box, CssBaseline, AppBar, Toolbar, Typography } from '@mui/material';
 import Navbar from './navbar/Navbar.jsx';
-import Home from './Pages/Home.jsx';
-import Checklist from './Pages/Checklist.jsx';
+import Home from './pages/home/Home.jsx';
+import Checklist from './pages/checklist/Checklist.jsx';
 
 class App extends React.Component {
   constructor(props) {
